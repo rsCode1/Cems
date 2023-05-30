13f48ccbd58f86a17deb9eaffba3de48ac8be48e
rsCode1 <100244697+rsCode1@users.noreply.github.com>
2023-05-31 01:41:56 +0300

    Add files via upload
    prototype

---
+++ CemsProject/CemsClient/src/gui/HDController.java
@@ -0,0 +1,19 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class HDController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ CemsProject/cemsServer/src/server/ServerUI.java
@@ -0,0 +1,32 @@
+package server;
+
+import gui.ServerStartScreenController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ServerUI extends Application {
+	
+	
+
+	public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/ServerStartScreen.fxml"));
+            Parent root = loader.load();
+            ServerStartScreenController controller = loader.getController();
+            EchoServer server = new EchoServer(5555);
+            controller.setServer(server);
+            server.setController(controller);
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
