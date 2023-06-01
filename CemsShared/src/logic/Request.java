affa6bbfa614d6c116333d41702b9c78e9777a0e
Roi Shamayev <roi222112@gmail.com>
2023-06-01 11:57:16 +0300

    added Request class

---
+++ CemsShared/src/logic/Request.java
@@ -0,0 +1,24 @@
+package logic;
+
+import java.io.Serializable;
+
+//Request class that holds the String requestType 
+//and the object that can be anything that you want to pass from server to Client or vice versa.
+public class Request implements Serializable {
+  private String requestType;
+  private Object requestParam;
+
+  public Request(String requestType, Object requestParam) {
+    this.requestType = requestType;
+    this.requestParam = requestParam;
+  }
+
+  public String getRequestType() {
+    return requestType;
+  }
+
+  public Object getRequestParam() {
+    return requestParam;
+  }
+
+}
