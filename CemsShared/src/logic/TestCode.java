ffb6bb255531d98b2d1785e15a7caeeac42579d5
nil-adar <niladar@gamil.com>
2023-06-13 01:47:48 +0300

    change

--- CemsShared/src/logic/TestCode.java
+++ CemsShared/src/logic/TestCode.java
@@ -4,14 +4,20 @@ import java.io.Serializable;
 
 public class TestCode implements Serializable {
 	private int code;
+	private int type;
+
+	public int getType() {
+		return type;
+	}
 
 	public int getCode() {
 		return code;
 	}
 
-	public TestCode(int code) {
+	public TestCode(int code,int type) {
 		super();
 		this.code = code;
+		this.type=type;
 	}
     
 }
