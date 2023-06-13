59ae4afa769b891a23b016b2f223ee446fcbb2f9
NitzanEz <ezra.nizan@gmail.com>
2023-06-13 03:23:02 +0300

    The endddddd

--- CemsShared/src/logic/Grades.java
+++ CemsShared/src/logic/Grades.java
@@ -3,73 +3,85 @@ package logic;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Grades implements Serializable  {
+public class Grades implements Serializable, Comparable<Grades> {
 	String examID;
 	String studentID;
 	String courseID;
 	int grade;
-	int dataOf;//student=0,lectuer=1,course=2
 	String lectuerID;
-	
+
 	public String getLectuerID() {
 		return lectuerID;
 	}
+
 	public void setLectuerID(String lectuerID) {
 		this.lectuerID = lectuerID;
 	}
+
 	public String getExamID() {
 		return examID;
 	}
+
 	public void setExamID(String examID) {
 		this.examID = examID;
 	}
+
 	public String getStudentID() {
 		return studentID;
 	}
-	
+
 	public String getCourseID() {
 		return courseID;
 	}
+
 	public void setCourseID(String courseID) {
 		this.courseID = courseID;
 	}
-	public int getDataOf() {
-		return dataOf;
-	}
-	public void setDataOf(int dataOf) {
-		this.dataOf = dataOf;
-	}
+
 	public int getGrade() {
 		return grade;
 	}
-	public Grades(String examID, String studentID, String courseID, int grade,int dataOf,String lectuerID) {
+
+	public Grades(String examID, String studentID, String courseID, int grade, String lectuerID) {
 		super();
 		this.examID = examID;
 		this.studentID = studentID;
 		this.courseID = courseID;
 		this.grade = grade;
-		this.dataOf=dataOf;
-		this.lectuerID=lectuerID;
-		}
+		this.lectuerID = lectuerID;
+	}
+
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
+
 	public void setStudentID(String studentID) {
 		this.studentID = studentID;
 	}
-	
+
 	public String getCourseName() {
 		return courseID;
 	}
+
 	public void setCourseName(String courseName) {
 		this.courseID = courseName;
 	}
+
 	@Override
 	public String toString() {
 		return "Grades [examID=" + examID + ", studentID=" + studentID + ", courseID=" + courseID + ", grade=" + grade
 				+ "]";
 	}
-	
 
+	@Override
+	public int compareTo(Grades o) {
+
+		if (this.getGrade() == (o.getGrade()) && (this.getStudentID().equals(o.getStudentID()))
+				&& (this.getCourseName().equals(o.getCourseName()))
+				&& (this.getLectuerID().equals(o.getLectuerID()) && this.getExamID().equals(o.getExamID())))
+			return 1;
+		else
+			return 0;
+	}
 
 }
