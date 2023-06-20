bf2d54bc360115fbcd171f2b52058fb921d40876
Roi Shamayev <roi222112@gmail.com>
2023-06-20 04:44:28 +0300

    add java doc files

---
+++ cemsServer/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"server","l":"EchoServer"},{"p":"server","l":"ServerCommandsHD"},{"p":"server","l":"ServerCommandsLecturer"},{"p":"server","l":"ServerCommandsStudent"},{"p":"gui","l":"ServerStartScreenController"},{"p":"server","l":"ServerUI"}]
